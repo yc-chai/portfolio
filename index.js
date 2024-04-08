57f0ee1367a68bfbde6c4856c69554677d8fa3fb
YongChen <yongchen99.work@gmail.com>
2024-04-08 23:45:26 +0800

    Update new version

--- index.js
+++
@@ -1,5 +0,0 @@
-var header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-    window.scrollY > 100 ? header.classList.add("backdrop") : header.classList.remove("backdrop");
-});
