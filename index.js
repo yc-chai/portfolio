eb79f7ed5a87a4a4722e880914dc848e7b5575d4
YongChen <yongchen99.work@gmail.com>
2024-04-09 12:21:04 +0800

    Update navigation scroll feature and CSS prefix

---
+++ index.js
@@ -0,0 +1,18 @@
+const navLinkEls = document.querySelectorAll(".nav-link");
+const sectionEls = document.querySelectorAll(".main-section");
+
+var currentSection = "about";
+window.addEventListener("scroll", () => {
+  sectionEls.forEach((sectionEl) => {
+    if (window.scrollY >= sectionEl.offsetTop - sectionEl.clientHeight / 4) {
+      currentSection = sectionEl.id;
+    }
+  });
+
+  navLinkEls.forEach((navLinkEl) => {
+    if (navLinkEl.href.includes(currentSection)) {
+      document.querySelector(".active").classList.remove("active");
+      navLinkEl.classList.add("active");
+    }
+  });
+});
