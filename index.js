2839eb34a758fa2ce1119984fc01e7e81a0def3c
yc-chai <chai99yc5219@gmail.com>
2025-05-23 20:32:15 +0800

    Enhance project and resume sections with new icons and styles for improved UI

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ function renderProjectsSection(projects) {
           <a href="${proj.link}" target="_blank" class="project-link"></a>
           <div class="details">
             <span class="inline-icon">
-              <svg xmlns="http://www.w3.org/2000/svg"><path d="m2.828 15.555 7.777-7.779L2.828 0 0 2.828l4.949 4.948L0 12.727l2.828 2.828z"/></svg>
+              <svg class="bold-right-arrow" xmlns="http://www.w3.org/2000/svg"><path d="m2.828 15.555 7.777-7.779L2.828 0 0 2.828l4.949 4.948L0 12.727l2.828 2.828z"/></svg>
               <h3 class="project-topic">${proj.topic}</h3>
             </span>
             <p class="project-description">${proj.description}</p>
